1058537834575b64b212a0d4900a91c6f2c97e62
male524 <male524@users.noreply.github.com>
2020-12-25 18:16:16 -0800

    test

--- Merge Sort.py
+++ Merge Sort.py
@@ -3,10 +3,10 @@ import random
 rand_items = [random.randint(-100, 100) for i in range(20)]
 
 
-def insertion_sort(A):
-
+def merge_sort(A):
+    pass
 
 
 print('before: ', rand_items)
-insertion_sort(rand_items)
+merge_sort(rand_items)
 print('After: ', rand_items)
