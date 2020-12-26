49d046fade81889223b41af6ec83db1f2c609354
male524 <male524@users.noreply.github.com>
2020-12-26 12:03:53 -0800

    Practiced merge sort.

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,10 +1,39 @@
 import random
 
-rand_items = [random.randint(-100, 100) for i in range(20)]
+rand_items = [random.randint(1, 100) for i in range(20)]
 
 
 def merge_sort(A):
-    pass
+    if len(A) > 1:
+        mid = len(A) // 2
+        L = A[:mid]
+        R = A[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                A[k] = L[i]
+                i += 1
+            else:
+                A[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            A[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            A[k] = R[j]
+            j += 1
+            k += 1
+
+
 
 
 print('before: ', rand_items)
